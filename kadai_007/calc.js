617efafbb4aa717dd18b01a30ae8a221f5e29112
Keieva <135431334+Keieva@users.noreply.github.com>
2023-06-21 13:53:58 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+let num =40;
+
+if (num % 15===0){
+ console.log('5と3の倍数です');
+}
+else if(num % 5===0){
+    console.log('5の倍数です');
+}
+else if(num % 3===0){
+    console.log('3の倍数です');
+}
+else{
+    console.log(num);
+}
